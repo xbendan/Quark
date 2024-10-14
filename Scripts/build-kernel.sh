ba0c4c1bd3b3bac8bd3327d27115225085c6c041
xbendan <richardc1ay@outlook.com>
2024-10-14 02:50:46 -0400

    Modify build scripts.

---
+++ Scripts/build-kernel.sh
@@ -0,0 +1,17 @@
+LIMINE_URL="https://github.com/limine-bootloader/limine.git"
+LIMINE_BRANCH="v8.x-binary"
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PATH_THIRDPARTY="$(realpath --relative-to="$SCRIPT_DIR" "$SCRIPT_DIR/../Thirdparty")"
+PATH_KERNEL="$(realpath --relative-to="$SCRIPT_DIR" "$SCRIPT_DIR/../Kernel")"
+PATH_BASE="$(realpath --relative-to="$SCRIPT_DIR" "$SCRIPT_DIR/../Base")"
+PATH_APPS="$(realpath --relative-to="$SCRIPT_DIR" "$SCRIPT_DIR/../Apps")"
+
+# Download and build Limine
+echo "Checking for limine..."
+if [ ! -d "$PATH_THIRDPARTY/limine" ]; then
+    git clone --depth 1 --branch "$LIMINE_BRANCH" "$LIMINE_URL" "$PATH_THIRDPARTY/limine"
+    cd "$PATH_THIRDPARTY/limine"
+    make
+    # make install
+fi
