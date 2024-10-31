491ea4d13aa9e9e4a3d6a4c1a9b13e5d37a39565
xbendan <richardc1ay@outlook.com>
2024-10-31 00:51:18 -0400

    Update panic functions.

--- Kernel/Include/mixins/std/assert.h
+++ Kernel/Include/mixins/std/assert.h
@@ -1,23 +1,13 @@
 #pragma once
 
-#include <mixins/fmt/format.h>
 #include <mixins/meta/error.h>
 #include <mixins/std/panic.h>
-
-template <typename... Args>
-static inline void
-MakeAssertion(bool expr, const char* msg = "Assertion failed", Args&&... args)
-{
-    if (!expr) {
-        Std::SystemPanic(msg, Std::forward<Args>(args)...);
-    }
-}
 
 static inline void
 MakeAssertion(bool expr, const char* msg = "Assertion failed")
 {
     if (!expr) {
-        Std::SystemPanic(msg);
+        Std::panic(msg);
     }
 }
 
@@ -25,6 +15,6 @@ static inline void
 MakeAssertion(bool expr, Error err)
 {
     if (!expr) {
-        Std::ThrowError(err);
+        Std::panic(err);
     }
 }
--- Kernel/Include/mixins/std/panic.h
+++ Kernel/Include/mixins/std/panic.h
@@ -1,21 +1,8 @@
 #pragma once
 
-#include <mixins/fmt/format.h>
 #include <mixins/meta/error.h>
-#include <mixins/std/string.h>
 
 namespace Std {
-    [[noreturn]] void ThrowError(Error err);
-
-    [[noreturn]] void SystemPanic(string msg);
-
-    template <typename... Args>
-        requires(fmt::Translatable<Std::RemoveCvRef<Args>> && ...)
-    [[noreturn]] void SystemPanic(string msg, Args... args)
-    {
-        u8                 buffer[1024];
-        BufferOutputStream buf(buffer, sizeof(buffer));
-        f(buf, (const char*)msg.Data(), Std::forward<Args>(args)...);
-        SystemPanic(string(buffer, string::LengthOf(buffer)));
-    }
+    [[noreturn]] void panic(Error err);
+    [[noreturn]] void panic(const char* msg);
 }
